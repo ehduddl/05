423db082e81983dd5fc5f4414334889ef4cffd88
ehduddl <inyj03@gmail.com>
2022-10-04 17:05:03 +0900

    03

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
-	int x;
-    printf("정수 하나를 입력하시오 : ");
-    scanf("%d",&x);
-    
-    if(x<0) 
-    x=-1*x;
-    printf("절대값은 %d 입니다.",x);
-
+	char c;
+	int num=0; 
+	printf("input a string : ");
+	while ( (c = getchar() ) != '\n' ) //입력문자가 개행문자가 나올때까지 반복
+	{
+	if (c>='0'&&c<='9')//입력된 글자가 숫자인가? 
+	num++;	//그렇다면 하나를 센다. 
+	}
+	printf("the number df digits is %d",num);
 	return 0;
 }
