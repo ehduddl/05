7240de9679957ffdbf0f91639c0aac3712f68053
ehduddl <inyj03@gmail.com>
2022-10-04 17:19:04 +0900

    04

--- main.c
+++ main.c
@@ -4,14 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int num=0; 
-	printf("input a string : ");
-	while ( (c = getchar() ) != '\n' ) //입력문자가 개행문자가 나올때까지 반복
-	{
-	if (c>='0'&&c<='9')//입력된 글자가 숫자인가? 
-	num++;	//그렇다면 하나를 센다. 
-	}
-	printf("the number df digits is %d",num);
+	int a,i,sum=0;
+    printf("input a number : ");
+    scanf("%d",&a);
+    
+	for(i=0;i<=a;i++)
+       sum=sum+i;
+   
+    printf("더하기 결과는 %d입니다.",sum);
 	return 0;
 }
